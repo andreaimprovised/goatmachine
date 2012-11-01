2e0b231b63bbee82709bdd37778e7d7ba24093ed
andyisimprovised <andy.is.improvised@gmail.com>
2012-11-01 16:05:13 -0700

    added support for adding custom gmail cookie

--- goat_machine_runner.py
+++ goat_machine_runner.py
@@ -7,8 +7,12 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 
 
-def get_os_x_firefox_driver():
-	profiles_path = os.path.join(os.environ.get("HOME"), "Library/Application Support/Firefox/Profiles/")
+OS_X_FF_PROFILE_ROOT = "Library/Application Support/Firefox/Profiles/"
+LINUX_FF_PROIFLE_ROOT = ".mozilla/firefox/"
+
+
+def get_firefox_driver_with_profile(root):
+	profiles_path = os.path.join(os.environ.get("HOME"), root)
 
 	uname = os.environ.get("USER")
 
@@ -28,6 +32,13 @@ def get_os_x_firefox_driver():
 	ff_profile = webdriver.FirefoxProfile(os.path.join(profiles_path, profile_path))
 
 	return webdriver.Firefox(firefox_profile=ff_profile)
+
+
+def get_firefox_driver_with_cookie(cookie):
+	firefox_driver = webdriver.Firefox()
+	firefox_driver.get("http://mail.google.com/goatingyourightnow")
+	firefox_driver.add_cookie(cookie)
+	return firefox_driver
 
 
 class GoatMachineRunner(object):
@@ -128,7 +139,13 @@ if __name__ == "__main__":
 
 	goat_url = "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hausziege_04.jpg/256px-Hausziege_04.jpg"
 
-	ff = get_os_x_firefox_driver()
+	# This is my yelp cookie -- it may or may not expire at some point.
+	# If so, copy your GX value into here.
+	cookie = dict(name='GX', value='DQAAAIUBAADEYLX6Ju-695dxHbW5aleG0QKWA1LlH6OU4YA7Ayru9HlF5rqDP8SJYW9PbEPcpM0wyj_p79y4MOJC-a9XSIvr5fJcbkSjBOhULLiytYhLlxFU4eJXsYkEWNU7aNi3MGnwAbaROc44IkOBPkCiwXFWdwhKGFkFTQo41TJvYdzXnfOUXTpa_DSb5bk6GXYZFBZ-q4XZJO9fu5SQ8Undr8TW6J70cTnJ8zf-VNSVFdoR_pSb-mguiIwwHLaR8m2bxDvoKiBcdyS95SbuqLTplUxmTf8B8mqvUn5j-ZqPI8LVEKCKhZ54flX0m02eyC6FTaafdnBi4dfJfNcr9dSQ6D6BAebz91ZM1B0_Sl-WY3nq2KmSy35YFf3Z87xS2EcyAaqtnZPQGepd0jWRHSCK5xZ5hhj4usOuCHhz0zIxAF3yAULTkIeAwYRJDdd-YXsV_1KlZUcGeNhy5nSgVt1FftkGufcG1rFc2bOmVOz-1vK3qqXAYIazKVUzkhXNoJqbagsnMAZahANeQDVYErfiVMSM', domain='mail.google.com')
+
+	ff = get_firefox_driver_with_cookie(cookie)
+
+	# ff = get_os_x_firefox_driver_with_profile()
 
 	GoatMachineRunner(ff, to, su, goat_url, dry_run=True).post_goat_mail()
 
