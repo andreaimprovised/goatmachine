3e0d5f06f15ec73321ebb944b8f8deb769e462d2
Ivan Malison <IvanMalison@gmail.com>
2012-11-01 19:57:17 -0700

    We can now get cookies from either firefox or chrome automatically.

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python
 import gx_cookie_extractor
-import image_fetcher
+# import image_fetcher
 import goat_machine
 
 
@@ -9,7 +10,7 @@ if __name__ == "__main__":
 	# image_fetcher.GoatSearcher("selenium goat")
 	# goat_url = image_fetch.get_my_goat_image()
 	goat_url = "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hausziege_04.jpg/256px-Hausziege_04.jpg"
-	cookie = gx_cookie_extractor.ChromeGXCookieExtractor().get_gx_cookie()
+	cookie = gx_cookie_extractor.GXCookieExtractorMeta.get_gx_cookie()
 	firefox_driver = goat_machine.get_firefox_driver_with_cookie(cookie)
 	goat_machine.GoatMachine(firefox_driver, to, su, goat_url, dry_run=True).post_goat_mail()
 	firefox_driver.quit()
