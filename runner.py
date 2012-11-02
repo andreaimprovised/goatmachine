0301d893685e5d2fe71d4b6812606a393373b82d
andyisimprovised <andy.is.improvised@gmail.com>
2012-11-01 17:09:13 -0700

    fixed the runner

--- runner.py
+++ runner.py
@@ -1,13 +1,10 @@
 import chrome_cookie_extracter
-import image_fetcher
 import goat_machine
 
 
 if __name__ == "__main__":
 	to = "goats@gmail.com"
 	su = "selenium goat"
-	image_fetcher.GoatSearcher("selenium goat")
-	# goat_url = image_fetch.get_my_goat_image()
 	goat_url = "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hausziege_04.jpg/256px-Hausziege_04.jpg"
 	cookie = chrome_cookie_extracter.ChromeCookieExtracter().get_gx_cookie()
 	firefox_driver = goat_machine.get_firefox_driver_with_cookie(cookie)
