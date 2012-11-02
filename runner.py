9961b63b7abeffe8e493d901ed0fe133369ceb35
Ivan Malison <IvanMalison@gmail.com>
2012-11-01 17:06:52 -0700

    Fixed the stuff I broke.

--- runner.py
+++ runner.py
@@ -1,4 +1,4 @@
-import chrome_cookie_extracter
+import gx_cookie_extractor
 import image_fetcher
 import goat_machine
 
@@ -6,10 +6,10 @@ import goat_machine
 if __name__ == "__main__":
 	to = "goats@gmail.com"
 	su = "selenium goat"
-	image_fetcher.GoatSearcher("selenium goat")
+	# image_fetcher.GoatSearcher("selenium goat")
 	# goat_url = image_fetch.get_my_goat_image()
 	goat_url = "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hausziege_04.jpg/256px-Hausziege_04.jpg"
-	cookie = chrome_cookie_extracter.ChromeCookieExtracter().get_gx_cookie()
+	cookie = gx_cookie_extractor.ChromeGXCookieExtractor().get_gx_cookie()
 	firefox_driver = goat_machine.get_firefox_driver_with_cookie(cookie)
 	goat_machine.GoatMachine(firefox_driver, to, su, goat_url, dry_run=True).post_goat_mail()
 	firefox_driver.quit()
